8e412e3044a10f980bee91140a98bd03bf28ac62
zouyonghong <641261604@qq.com>
2021-05-11 13:10:08 +0800

    优化

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Linking, Platform, NativeModules } from 'react-native'
 const RNMarketCommentModule = NativeModules.RNMarketCommentModule
 export default function OpenAppStore(appId) {
     if (Platform.OS === 'ios') {
-        const url = `itms-apps://itunes.apple.com/cn/app/id${appId}?mt=8&action=write-review`
+        const url = `itms-apps://itunes.apple.com/cn/app/id${appId}?mt=8`
         return Linking.openURL(url).catch(err => console.error('An error occurred', err))
     }
     RNMarketCommentModule.openAppStore()
